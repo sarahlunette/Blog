42e3d2357c7620ab01ab292147c78d19cd314f82
Your Name <you@example.com>
2025-12-21 16:29:24 +0100

    first commit

---
+++ Presentation.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.title('TheLab blogpost')
+st.header('This blog is designed to gather articles and interviews on firms of emergency crise, disaster management and prevention, and healthcare or environment. Most of them are eyeing or using AI and I believe they are to be followed.')
+
