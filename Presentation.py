d598e486fc2c4b8bdf626d54a3d5985982c27c18
Sarah Le Net <sarahlunette@users.noreply.github.com>
2026-01-12 11:07:40 +0100

    black

--- Presentation.py
+++ Presentation.py
@@ -1,5 +1,6 @@
 import streamlit as st
 
-st.title('TheLab blogpost')
-st.write('This blog is designed to gather articles and interviews on firms of emergency crise, disaster management and prevention, and healthcare or the environment. Most of them are eyeing or using AI and I believe they are to be followed.')
-
+st.title("TheLab blogpost")
+st.write(
+    "This blog is designed to gather articles and interviews on firms of emergency crise, disaster management and prevention, and healthcare or the environment. Most of them are eyeing or using AI and I believe they are to be followed."
+)
